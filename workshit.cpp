291a58a788d76c1f6a6cca54613472787a9f2e9a
Lazarus_W <your-username@user.noreply.github.com>
2024-10-08 16:18:07 -0300

    workshit.cpp

---
+++ workshit.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+int main(){
+    /*
+    int x;
+    cout<<"inter a number /n";
+    cin>>x;
+    if(x%2==0){
+        cout<<x<<" is even number.";
+    }
+    else{
+        cout<<x<<" is odd numebr.";
+    }
+   int num1; 
+   int num2;
+   int num3;
+   cout<<"inter two number: "<<"\n";
+   cin>>num1>>num2;
+   if(num1<num2){
+    cout<<num2<<"\t"<<"is the largest number.";
+   }
+   else if(num1>num2){
+    cout<<num1<<"\t"<<"is the largest number.";
+   }
+   else{
+    cout<<"both number's are equal.";
+   }
+
+cout<<"inter 3 rundom number's \n";
+cin>>num1>>num2>>num3;
+
+   int smalles=num1;
+if(num2<smalles){
+    smalles=num2;
+    }
+else if(num3<smalles){
+    smalles=num3;
+}    
+int largest=num1;
+if(num2>largest){
+    largest=num2;
+    }
+else if(num3>largest){
+    largest=num3;
+}
+double midnum=num1+num2+num3-smalles-largest;
+cout<<"the assending order of the numbers you intered is: "<<smalles<<","<<midnum<<","<<largest;
+*/
+
+return 0;
+}
